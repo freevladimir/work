25775511f6e5d6b7704bb846e90c24c47f66c3b6
freevladimir <33995040+freevladimir@users.noreply.github.com>
2020-05-25 16:14:48 +0300

    Add files via upload

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -318,12 +318,42 @@ router.get('/game', auth,
 router.get('/friends', auth,
     async (req, res)=>{
         try{
+            // const userAddress = await User.find({_id: req.user.userId})
+            // console.log("userAddress: ", userAddress[0].wallet)
+            // const userData = await User.find({friendId: userAddress[0].wallet})
+            // res.json(userData)
+            // console.log("Friends")
+            // console.log(userData)
+            console.log('start getting friends')
             const userAddress = await User.find({_id: req.user.userId})
             console.log("userAddress: ", userAddress[0].wallet)
-            const userData = await User.find({friendId: userAddress[0].wallet})
+            let refContract = "0x6E712549EFE403866BCd2D405f66672DD59a90D4"
+            let refAbi = [{"constant":false,"inputs":[{"name":"_contr","type":"address"}],"name":"addContract","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_referrer","type":"address"}],"name":"addReferer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_refCashbox","type":"address"}],"name":"changeRefCashbox","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_newManager","type":"address"}],"name":"setManagment","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_sevenTOP","type":"address"}],"name":"setSevenTOP","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_storageLimitLottery","type":"address"}],"name":"setStorageLimitLottery","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"player","type":"address"},{"indexed":false,"name":"referrer","type":"address"}],"name":"ReferrerAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"contracts","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_user","type":"address"}],"name":"getReferal","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_user","type":"address"}],"name":"getReferers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isOwner","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"management","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"refCashbox","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"sevenTOP","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"storageLimitLottery","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]
+            let RefStorage = new web3.eth.Contract(refAbi, refContract)
+            let userData = []
+            let friendsAddresses = await RefStorage.methods.getReferers(userAddress[0].wallet).call({}, (err, res) => {
+                if (res) {
+                    return res
+                } else if (err) {
+                    console.log("This is error: ", err);
+                    return null;
+                }
+            })
+            console.log('friendsAddresses: ', friendsAddresses)
+
+            for(let i=0; i<friendsAddresses.length; i++){
+                let result = await User.find({wallet: friendsAddresses[i]})
+                if(!result.length){
+                    let data = {_id: 'undefined', name: (friendsAddresses[i].substr(0, 6) + "..." + friendsAddresses[i].substr(38, 4))}
+                    userData.push(data)
+                } else {
+                    userData.push(result[0])
+                }
+            }
             res.json(userData)
             console.log("Friends")
             console.log(userData)
+
         } catch (e) {
             res.status(500).json({message: 'Something go wrong, try again'})
             console.log(e)
@@ -334,7 +364,19 @@ router.get('/allusers', auth,
     async (req, res)=>{
         try{
             const user = await User.find({_id: req.user.userId})
-            const _friends = await User.find({friendId: user[0].wallet})
+            // const _friends = await User.find({friendId: user[0].wallet})
+            let refContract = "0x6E712549EFE403866BCd2D405f66672DD59a90D4"
+            let refAbi = [{"constant":false,"inputs":[{"name":"_contr","type":"address"}],"name":"addContract","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_referrer","type":"address"}],"name":"addReferer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_refCashbox","type":"address"}],"name":"changeRefCashbox","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_newManager","type":"address"}],"name":"setManagment","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_sevenTOP","type":"address"}],"name":"setSevenTOP","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_storageLimitLottery","type":"address"}],"name":"setStorageLimitLottery","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"player","type":"address"},{"indexed":false,"name":"referrer","type":"address"}],"name":"ReferrerAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"contracts","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_user","type":"address"}],"name":"getReferal","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_user","type":"address"}],"name":"getReferers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isOwner","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"management","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"refCashbox","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"sevenTOP","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"storageLimitLottery","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]
+            let RefStorage = new web3.eth.Contract(refAbi, refContract)
+            const _friends = await RefStorage.methods.getReferers(user[0].wallet).call({}, (err, res) => {
+                if (res) {
+                    console.log('friends count: ', res)
+                    return res
+                } else if (err) {
+                    console.log("This is error: ", err);
+                    return [];
+                }
+            })
             const _allUsers = await User.find()
             res.json({friends: _friends.length, allUsers: _allUsers.length})
             console.log("Friends")
@@ -350,12 +392,13 @@ router.post('/members',
 
     async (req, res)=>{
         try{
-            let members = req.body[0]
-            console.log('req.body: ', members)
+            let members = req.body
+            console.log('req.body members: ', members)
 
             let users = []
             for(let i=0; i<members.length; i++){
                 let user = await User.findOne({wallet: members[i]})
+                console.log('cc')
                 console.log(user)
                 if(!user){
                     users.push({id: 'undefined', name: members[i].substr(0, 6) + "..." + members[i].substr(38, 4)})
@@ -390,7 +433,7 @@ router.post('/winners',
                 let user = await User.findOne({wallet: members[i][0]})
                 console.log(user)
                 if(!user){
-                    users.push({ name: members[i].substr(0, 6) + "..." + members[i].substr(38, 4), time: members[i][1], sum: members[i][2], id: 'undefined'})
+                    users.push({ name: members[i][0].substr(0, 6) + "..." + members[i][0].substr(38, 4), time: members[i][1], sum: members[i][2], id: 'undefined'})
                 } else{
                     users.push({name: user.name, time: members[i][1], sum: members[i][2], id: user._id})
                 }
