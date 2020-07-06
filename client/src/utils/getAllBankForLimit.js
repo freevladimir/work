3e91ef64fe370b88f81ea6a13b988cf5a68c2c8f
freevladimir <smagazhevskijj@gmail.com>
2020-07-06 16:59:13 +0300

    First commit

--- client/src/utils/getAllBankForLimit.js
+++ client/src/utils/getAllBankForLimit.js
@@ -19,48 +19,48 @@ const getAllBankOfLimitGame = async () => {
                 }
             });
         }
-        for (let i = 0; i < contractAdresses["5Minutes"].addresses.length; i++) {
-            const Lottery = new web3.eth.Contract(contractAdresses["5Minutes"].abi, contractAdresses["5Minutes"].addresses[i]["addressValue"]);
-            await Lottery.methods.getSumOnContract().call({}, (err, res) => {
-                if (res) {
-                    if(result[1]){
-                        result[1] += parseInt(res);
-                    } else {
-                        result[1] = parseInt(res);
-                    }
-                } else if (err) {
-                    console.log("This is error: ", err);
-                }
-            });
-        }
-        for (let i = 0; i < contractAdresses["hour"].addresses.length; i++) {
-            const Lottery = new web3.eth.Contract(contractAdresses["hour"].abi, contractAdresses["hour"].addresses[i]["addressValue"]);
-            await Lottery.methods.getSumOnContract().call({}, (err, res) => {
-                if (res) {
-                    if(result[2]){
-                        result[2] += parseInt(res);
-                    } else {
-                        result[2] = parseInt(res);
-                    }
-                } else if (err) {
-                    console.log("This is error: ", err);
-                }
-            });
-        }
-        for (let i = 0; i < contractAdresses["day"].addresses.length; i++) {
-            const Lottery = new web3.eth.Contract(contractAdresses["day"].abi, contractAdresses["day"].addresses[i]["addressValue"]);
-            await Lottery.methods.getSumOnContract().call({}, (err, res) => {
-                if (res) {
-                    if(result[3]){
-                        result[3] += parseInt(res);
-                    } else {
-                        result[3] = parseInt(res);
-                    }
-                } else if (err) {
-                    console.log("This is error: ", err);
-                }
-            });
-        }
+        // for (let i = 0; i < contractAdresses["5Minutes"].addresses.length; i++) {
+        //     const Lottery = new web3.eth.Contract(contractAdresses["5Minutes"].abi, contractAdresses["5Minutes"].addresses[i]["addressValue"]);
+        //     await Lottery.methods.getSumOnContract().call({}, (err, res) => {
+        //         if (res) {
+        //             if(result[1]){
+        //                 result[1] += parseInt(res);
+        //             } else {
+        //                 result[1] = parseInt(res);
+        //             }
+        //         } else if (err) {
+        //             console.log("This is error: ", err);
+        //         }
+        //     });
+        // }
+        // for (let i = 0; i < contractAdresses["hour"].addresses.length; i++) {
+        //     const Lottery = new web3.eth.Contract(contractAdresses["hour"].abi, contractAdresses["hour"].addresses[i]["addressValue"]);
+        //     await Lottery.methods.getSumOnContract().call({}, (err, res) => {
+        //         if (res) {
+        //             if(result[2]){
+        //                 result[2] += parseInt(res);
+        //             } else {
+        //                 result[2] = parseInt(res);
+        //             }
+        //         } else if (err) {
+        //             console.log("This is error: ", err);
+        //         }
+        //     });
+        // }
+        // for (let i = 0; i < contractAdresses["day"].addresses.length; i++) {
+        //     const Lottery = new web3.eth.Contract(contractAdresses["day"].abi, contractAdresses["day"].addresses[i]["addressValue"]);
+        //     await Lottery.methods.getSumOnContract().call({}, (err, res) => {
+        //         if (res) {
+        //             if(result[3]){
+        //                 result[3] += parseInt(res);
+        //             } else {
+        //                 result[3] = parseInt(res);
+        //             }
+        //         } else if (err) {
+        //             console.log("This is error: ", err);
+        //         }
+        //     });
+        // }
         for (let i = 0; i < contractAdresses["week"].addresses.length; i++) {
             const Lottery = new web3.eth.Contract(contractAdresses["week"].abi, contractAdresses["week"].addresses[i]["addressValue"]);
             await Lottery.methods.getSumOnContract().call({}, (err, res) => {
--- client/src/utils/getAllTimeEnds.js
+++ client/src/utils/getAllTimeEnds.js
@@ -4,49 +4,49 @@ import config from "../config/default";
 const getAllTimesEndGame = async (lottery) => {
     let result = [];
     if (web3) {
-        const address5Minutes = config['5Minutes'].addresses[0].addressValue
-        const addressHour = config['hour'].addresses[0].addressValue
-        const addressDay = config['day'].addresses[0].addressValue
+        // const address5Minutes = config['5Minutes'].addresses[0].addressValue
+        // const addressHour = config['hour'].addresses[0].addressValue
+        // const addressDay = config['day'].addresses[0].addressValue
         const addressWeek = config['week'].addresses[0].addressValue
         const addressMonth = config['month'].addresses[0].addressValue
         const addressYear = config['year'].addresses[0].addressValue
 
         const abi = config['5Minutes'].abi;
-        const FiveMinutes = new web3.eth.Contract(abi, address5Minutes);
-        await FiveMinutes.methods.getTimeEnd().call({}, (err, res) => {
-            if (res) {
-                result.push(parseInt(res - Date.now()/1000)*1000)
-                if (result[0]<=0) result[0] = 0
-                console.log('TimeEnd: ', result)
-            } else if (err) {
-                console.log("This is error: ", err);
-                result = null;
-            }
-        });
+        // const FiveMinutes = new web3.eth.Contract(abi, address5Minutes);
+        // await FiveMinutes.methods.getTimeEnd().call({}, (err, res) => {
+        //     if (res) {
+        //         result.push(parseInt(res - Date.now()/1000)*1000)
+        //         if (result[0]<=0) result[0] = 0
+        //         console.log('TimeEnd: ', result)
+        //     } else if (err) {
+        //         console.log("This is error: ", err);
+        //         result = null;
+        //     }
+        // });
 
-        const Hour = new web3.eth.Contract(abi, addressHour);
-        await Hour.methods.getTimeEnd().call({}, (err, res) => {
-            if (res) {
-                result.push(parseInt(res - Date.now()/1000)*1000)
-                if (result[1]<=0) result[1] = 0
-                console.log('TimeEnd: ', result)
-            } else if (err) {
-                console.log("This is error: ", err);
-                result = null;
-            }
-        });
+        // const Hour = new web3.eth.Contract(abi, addressHour);
+        // await Hour.methods.getTimeEnd().call({}, (err, res) => {
+        //     if (res) {
+        //         result.push(parseInt(res - Date.now()/1000)*1000)
+        //         if (result[1]<=0) result[1] = 0
+        //         console.log('TimeEnd: ', result)
+        //     } else if (err) {
+        //         console.log("This is error: ", err);
+        //         result = null;
+        //     }
+        // });
 
-        const Day = new web3.eth.Contract(abi, addressDay);
-        await Day.methods.getTimeEnd().call({}, (err, res) => {
-            if (res) {
-                result.push(parseInt(res - Date.now()/1000)*1000)
-                if (result[2]<=0) result[2] = 0
-                console.log('TimeEnd: ', result)
-            } else if (err) {
-                console.log("This is error: ", err);
-                result = null;
-            }
-        });
+        // const Day = new web3.eth.Contract(abi, addressDay);
+        // await Day.methods.getTimeEnd().call({}, (err, res) => {
+        //     if (res) {
+        //         result.push(parseInt(res - Date.now()/1000)*1000)
+        //         if (result[2]<=0) result[2] = 0
+        //         console.log('TimeEnd: ', result)
+        //     } else if (err) {
+        //         console.log("This is error: ", err);
+        //         result = null;
+        //     }
+        // });
 
         const Week = new web3.eth.Contract(abi, addressWeek);
         await Week.methods.getTimeEnd().call({}, (err, res) => {
