277da9445376256869223164732ed640590a02dd
freevladimir <smagazhevskijj@gmail.com>
2020-07-06 19:29:26 +0300

    First commit

--- client/src/utils/getAllCountOfTickets.js
+++ client/src/utils/getAllCountOfTickets.js
@@ -19,8 +19,50 @@ const getAllCountOfTickets = async () => {
                 }
             });
         }
-        for (let i = 0; i < contractAdresses["5Minutes"].addresses.length; i++) {
-            const Lottery = new web3.eth.Contract(contractAdresses["5Minutes"].abi, contractAdresses["5Minutes"].addresses[i]["addressValue"]);
+        // for (let i = 0; i < contractAdresses["5Minutes"].addresses.length; i++) {
+        //     const Lottery = new web3.eth.Contract(contractAdresses["5Minutes"].abi, contractAdresses["5Minutes"].addresses[i]["addressValue"]);
+        //     await Lottery.methods.getTicketsLength().call({}, (err, res) => {
+        //         if (res) {
+        //             if(result[1]){
+        //                 result[1] += parseInt(res);
+        //             } else {
+        //                 result[1] = parseInt(res);
+        //             }
+        //         } else if (err) {
+        //             console.log("This is error: ", err);
+        //         }
+        //     });
+        // }
+        // for (let i = 0; i < contractAdresses["hour"].addresses.length; i++) {
+        //     const Lottery = new web3.eth.Contract(contractAdresses["hour"].abi, contractAdresses["hour"].addresses[i]["addressValue"]);
+        //     await Lottery.methods.getTicketsLength().call({}, (err, res) => {
+        //         if (res) {
+        //             if(result[2]){
+        //                 result[2] += parseInt(res);
+        //             } else {
+        //                 result[2] = parseInt(res);
+        //             }
+        //         } else if (err) {
+        //             console.log("This is error: ", err);
+        //         }
+        //     });
+        // }
+        // for (let i = 0; i < contractAdresses["day"].addresses.length; i++) {
+        //     const Lottery = new web3.eth.Contract(contractAdresses["day"].abi, contractAdresses["day"].addresses[i]["addressValue"]);
+        //     await Lottery.methods.getTicketsLength().call({}, (err, res) => {
+        //         if (res) {
+        //             if(result[3]){
+        //                 result[3] += parseInt(res);
+        //             } else {
+        //                 result[3] = parseInt(res);
+        //             }
+        //         } else if (err) {
+        //             console.log("This is error: ", err);
+        //         }
+        //     });
+        // }
+        for (let i = 0; i < contractAdresses["week"].addresses.length; i++) {
+            const Lottery = new web3.eth.Contract(contractAdresses["week"].abi, contractAdresses["week"].addresses[i]["addressValue"]);
             await Lottery.methods.getTicketsLength().call({}, (err, res) => {
                 if (res) {
                     if(result[1]){
@@ -33,8 +75,8 @@ const getAllCountOfTickets = async () => {
                 }
             });
         }
-        for (let i = 0; i < contractAdresses["hour"].addresses.length; i++) {
-            const Lottery = new web3.eth.Contract(contractAdresses["hour"].abi, contractAdresses["hour"].addresses[i]["addressValue"]);
+        for (let i = 0; i < contractAdresses["month"].addresses.length; i++) {
+            const Lottery = new web3.eth.Contract(contractAdresses["month"].abi, contractAdresses["month"].addresses[i]["addressValue"]);
             await Lottery.methods.getTicketsLength().call({}, (err, res) => {
                 if (res) {
                     if(result[2]){
@@ -47,56 +89,14 @@ const getAllCountOfTickets = async () => {
                 }
             });
         }
-        for (let i = 0; i < contractAdresses["day"].addresses.length; i++) {
-            const Lottery = new web3.eth.Contract(contractAdresses["day"].abi, contractAdresses["day"].addresses[i]["addressValue"]);
-            await Lottery.methods.getTicketsLength().call({}, (err, res) => {
-                if (res) {
-                    if(result[3]){
-                        result[3] += parseInt(res);
-                    } else {
-                        result[3] = parseInt(res);
-                    }
-                } else if (err) {
-                    console.log("This is error: ", err);
-                }
-            });
-        }
-        for (let i = 0; i < contractAdresses["week"].addresses.length; i++) {
-            const Lottery = new web3.eth.Contract(contractAdresses["week"].abi, contractAdresses["week"].addresses[i]["addressValue"]);
-            await Lottery.methods.getTicketsLength().call({}, (err, res) => {
-                if (res) {
-                    if(result[4]){
-                        result[4] += parseInt(res);
-                    } else {
-                        result[4] = parseInt(res);
-                    }
-                } else if (err) {
-                    console.log("This is error: ", err);
-                }
-            });
-        }
-        for (let i = 0; i < contractAdresses["month"].addresses.length; i++) {
-            const Lottery = new web3.eth.Contract(contractAdresses["month"].abi, contractAdresses["month"].addresses[i]["addressValue"]);
-            await Lottery.methods.getTicketsLength().call({}, (err, res) => {
-                if (res) {
-                    if(result[5]){
-                        result[5] += parseInt(res);
-                    } else {
-                        result[5] = parseInt(res);
-                    }
-                } else if (err) {
-                    console.log("This is error: ", err);
-                }
-            });
-        }
         for (let i = 0; i < contractAdresses["year"].addresses.length; i++) {
             const Lottery = new web3.eth.Contract(contractAdresses["year"].abi, contractAdresses["year"].addresses[i]["addressValue"]);
             await Lottery.methods.getTicketsLength().call({}, (err, res) => {
                 if (res) {
-                    if(result[6]){
-                        result[6] += parseInt(res);
+                    if(result[2]){
+                        result[2] += parseInt(res);
                     } else {
-                        result[6] = parseInt(res);
+                        result[2] = parseInt(res);
                     }
                 } else if (err) {
                     console.log("This is error: ", err);
