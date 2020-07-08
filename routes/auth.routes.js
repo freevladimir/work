2fe0020c50b60f6a8463c3627eb2fb0adee29b62
freevladimir <smagazhevskijj@gmail.com>
2020-07-08 12:18:28 +0300

    'commit'

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -393,7 +393,7 @@ router.get('/allusers', auth,
                 }
             })
             const _allUsers = await User.find()
-            res.json({friends: _friends.length, allUsers: _allUsers.length, allUsers2: allusers})
+            res.json({friends: _friends.length, allUsers: _allUsers.length, allUsers2: _allusers})
             console.log("Friends")
             console.log(userData)
         } catch (e) {
